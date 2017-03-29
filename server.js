08daf9f639e34e3956a753b2c8249b9b2f6d432b
wshuang6 <seraph617@gmail.com>
2017-03-29 16:11:23 -0400

    test 3 test not as hard

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyparser = require('body-parser');
 // let toDos = [];
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  // res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
