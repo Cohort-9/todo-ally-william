83e1985cf1dafb47ea95faad60b07157cb2a0ed0
wshuang6 <seraph617@gmail.com>
2017-03-29 15:47:31 -0400

    test again

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
