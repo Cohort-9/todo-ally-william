74fa6ee601fdb18ea206cdbf29b7f0ffaa711adf
wshuang6 <seraph617@gmail.com>
2017-03-29 15:48:20 -0400

    test 3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  // res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
@@ -17,9 +17,9 @@ app.get('/', (req, res) =>{
     res.send('toDos');
 });
 
-app.delete('/', (req, res) => {
-  res.status(204).send('Success');
-});
+// app.delete('/', (req, res) => {
+//   res.status(204).send('Success');
+// });
 
 app.listen(process.env.PORT || 8080);
 
