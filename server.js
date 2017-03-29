24dec9ffbce96ee7f2f39d66d96a82e107cd4824
wshuang6 <seraph617@gmail.com>
2017-03-29 16:07:53 -0400

    testestest

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const app = express();
 const bodyparser = require('body-parser');
 
 // let toDos = [];
-
-app.use(express.static('public'));
-app.use(bodyparser.json());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+// app.use(express.static('public'));
+app.use(bodyparser.json());
+
 
 app.get('/', (req, res) =>{
     res.send('toDos');
