68c2ed183db1c54f099321989c2e2ef77768b4c3
Alvin Law <alvinlaw@gmail.com>
2017-03-29 11:54:58 -0700

    Setup CORS and app.get at home directory, add env, update gitigonre

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) =>{
+    res.send('This is a get endpoint');
+});
+
 app.listen(process.env.PORT || 8080);
 
 exports.app = app;
