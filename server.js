11821b7bb167f5aad4ad3a105a8de026fc1495cb
wshuang6 <seraph617@gmail.com>
2017-03-29 16:03:08 -0400

    testest

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.use(bodyparser.json());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  // res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
@@ -21,7 +21,7 @@ app.get('/', (req, res) =>{
 
 app.post('/', (req, res) => {
   res.send(req.body);
-})
+});
 // app.delete('/', (req, res) => {
 //   res.status(204).json({message: 'success'});
 // });
